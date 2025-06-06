acbfd863c74e56cbfd113696855162ebf9f3ccb4
Munni23-13 <shaiknagurbasha803@gmail.com>
2025-06-06 07:14:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import WelcomeScreen from './pages/WelcomeScreen';
-import LoginScreen from './pages/LoginScreen';
-import RegisterScreen from './pages/RegisterScreen';
-import AccountSettingsCard from './pages/AccountSettingsCard';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-hook';
+import WelcomeScreen from './components/WelcomeScreen';
+import LoginScreen from './components/LoginScreen';
+import RegisterScreen from './components/RegisterScreen';
+import AccountSettingsCard from './components/AccountSettingsCard';
 
 function App() {
   return (
